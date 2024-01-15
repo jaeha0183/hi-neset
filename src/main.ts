1924ce8b0edab9dc4828487e6434f3229a1d2a1b
Jaeha Lee <jaeha0183@gmail.com>
2024-01-15 19:38:39 +0900

    [#1] Feat : "DTOs and Validation 1"

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 데코레이터가 없는 속성은 거름
+      forbidNonWhitelisted: true, // 데코레이터가 없는 속성이 있으면 요청 자체를 막음
+      transform: true, // url에서 받아온 string을 number로 변환
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
