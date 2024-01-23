869d3d6f003750a01975bcae830933383c83910d
Jaeha Lee <jaeha0183@gmail.com>
2024-01-23 15:45:20 +0900

    [#1] Fix : "Modules and Dependency Injection"

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,10 +1,13 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateMovieDto {
   @IsString()
   readonly title: string;
+
   @IsNumber()
   readonly year: number;
+
+  @IsOptional() // 데코레이터가 없는 속성은 거름
   @IsString({ each: true })
   readonly genres: string[];
 }
